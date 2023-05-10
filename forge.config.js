0725f48606a98df4730c726cb86ca3a94679cd0b
tanykazy <tanykazy@gmail.com>
2023-05-10 23:04:13 +0900

    Add @electron-forge/publisher-github

--- forge.config.js
+++ forge.config.js
@@ -19,4 +19,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
 };
